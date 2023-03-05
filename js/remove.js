27ce4c8b10479e1f05368adc3dfc751196a83979
bedesignermonir <clkmonir@gmail.com>
2023-03-05 15:52:59 +0600

    remove and minus added

---
+++ js/remove.js
@@ -0,0 +1,4 @@
+document.getElementById('phone-remove-button').addEventListener('click', function () {
+    const displayNone = document.getElementById('phone-section');
+    displayNone.style.display = 'none';
+})
---
+++ js/substract.js
@@ -0,0 +1,12 @@
+document.getElementById('mobile-substract-button').addEventListener('click', function () {
+    const mobileCount = document.getElementById('mobile-count');
+    const mobileCountNum = parseInt(mobileCount.value);
+    const mobileCountIncrease = mobileCountNum - 1;
+    mobileCount.value = mobileCountIncrease;
+
+
+    const mobilePrice = document.getElementById('mobile-price');
+    const mobilePriceNum = parseFloat(mobilePrice.innerText);
+    console.log(mobilePriceNum);
+
+})
