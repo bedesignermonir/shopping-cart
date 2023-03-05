2d45225c8e1862b11dacca83d803de564dc1a134
bedesignermonir <clkmonir@gmail.com>
2023-03-05 18:09:34 +0600

    js update

--- js/add.js
+++ js/add.js
@@ -7,6 +7,28 @@ document.getElementById('mobile-added-button').addEventListener('click', functio
 
     const mobilePrice = document.getElementById('mobile-price');
     const mobilePriceNum = parseFloat(mobilePrice.innerText);
-    console.log(mobilePriceNum);
+    const doubleprice = mobilePriceNum * mobileCountIncrease;
+
+
+    mobilePrice.innerText = doubleprice;
+    console.log(doubleprice)
+
+
+});
+document.getElementById('mobile-substract-button').addEventListener('click', function () {
+    const mobileCount = document.getElementById('mobile-count');
+    const mobileCountNum = parseInt(mobileCount.value);
+    const mobileCountIncrease = mobileCountNum - 1;
+    mobileCount.value = mobileCountIncrease;
+
+
+    const mobilePrice = document.getElementById('mobile-price');
+    const mobilePriceNum = parseFloat(mobilePrice.innerText);
+    const doubleprice = mobilePriceNum * mobileCountIncrease;
+
+
+    mobilePrice.innerText = doubleprice;
+    console.log(doubleprice)
+
 
 })
