73da8b83db2738290ff9409afeeaa9797862da24
bedesignermonir <clkmonir@gmail.com>
2023-03-06 09:44:54 +0600

    ui update

--- js/remove.js
+++ js/remove.js
@@ -2,3 +2,8 @@ document.getElementById('phone-remove-button').addEventListener('click', functio
     const displayNone = document.getElementById('phone-section');
     displayNone.style.display = 'none';
 })
+
+document.getElementById('phone-cover-remove-button').addEventListener('click', function () {
+    const displayNone = document.getElementById('phone-cover');
+    displayNone.style.display = 'none';
+})
